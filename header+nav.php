eff1c2c808a37e67588eda4a8935867a94516571
IbukunOluwa Shotubo <mcblessing1@gmail.com>
2018-06-12 21:13:19 +0100

    Fixed header issues.

--- header+nav.php
+++ header+nav.php
@@ -1,136 +1,130 @@
 <!-- whole page -->
 
 <div class="container-fluid">
-		<div class="row">
-			<!-- Left pane - Nav: Logo and Menu -->
-			<div class="col-md-3 left-pane">
+	<div class="row">
+		<!-- Left pane - Nav: Logo and Menu -->
+		<div class="col-md-3 left-pane">
 
-				<!-- Logo -->
-				<div class="row">
-					<div class="col-md logo text-center">
-						<img src="http://via.placeholder.com/100" alt="Adullam Logo">
-					</div>
-				</div>
-
-				<!-- Menu Nav -->
-				<div class="row">
-					<div class="col-md main-menu">
-						<div class="menu-button" id="menu-button">
-							<i class="fa fa-bars fa-2x">
-							</i>
-						</div>
-						<h5>MAIN MENU</h5>
-						<div class="menu-links is-hidden">
-							<ul>
-								<li>
-									<i class="fa fa-home fa-2x"></i>
-									<a href="home.php">Home</a>
-								</li>
-								<li>
-									<i class="fa fa-tag fa-2x"></i>
-									<a href="">About Adullam</a>
-								</li>
-								<li>
-									<i class="fa fa-group fa-2x"></i>
-									<a href="">Interest Groups</a>
-								</li>
-								<li>
-									<i class="fa fa-sitemap fa-2x"></i>
-									<a href="">Adullam Teams</a>
-								</li>
-								<li>
-									<i class="fa fa-handshake-o fa-2x"></i>
-									<a href="">Opportunities</a>
-								</li>
-								<li>
-									<i class="fa fa-film fa-2x"></i>
-									<a href="">Gallery</a>
-								</li>
-								<li>
-									<i class="fa fa-calendar fa-2x"></i>
-									<a href="">Event Calender</a>
-								</li>
-								<li>
-									<i class="fa fa-hdd-o fa-2x"></i>
-									<a href="">Resource Bank</a>
-								</li>
-								<li>
-									<i class="fa fa-comments fa-2x"></i>
-									<a href="">Adullam Chat</a>
-								</li>
-							</ul>
-						</div>
-					</div>
+			<!-- Logo -->
+			<div class="row">
+				<div class=" logo text-center">
+					<img class="logo-img" src="/assets/images/Logo-Full-Colour.png" alt="Adullam Logo">
 				</div>
 			</div>
 
-			
-			
-			<!-- Right pane - Content Pane -->
-			<div class="col-md-9 right-pane">
+			<!-- Menu Nav -->
+			<div class="row">
+				<div class="col-md main-menu">
+					<div class="menu-button" id="menu-button">
+						<i class="fa fa-bars fa-2x">
+							</i>
+					</div>
+					<h5 class="main-menu-text">MAIN MENU</h5>
+					<div class="menu-links is-hidden">
+						<ul class="main-menu-ul">
+							<li>
+								<i class="fa fa-home fa-2x"></i>
+								<a href="home.php">Home</a>
+							</li>
+							<li>
+								<i class="fa fa-tag fa-2x"></i>
+								<a href="">About Adullam</a>
+							</li>
+							<li>
+								<i class="fa fa-group fa-2x"></i>
+								<a href="">Interest Groups</a>
+							</li>
+							<li>
+								<i class="fa fa-sitemap fa-2x"></i>
+								<a href="">Adullam Teams</a>
+							</li>
+							<li>
+								<i class="fa fa-handshake-o fa-2x"></i>
+								<a href="">Opportunities</a>
+							</li>
+							<li>
+								<i class="fa fa-film fa-2x"></i>
+								<a href="">Gallery</a>
+							</li>
+							<li>
+								<i class="fa fa-calendar fa-2x"></i>
+								<a href="">Event Calender</a>
+							</li>
+							<li>
+								<i class="fa fa-hdd-o fa-2x"></i>
+								<a href="">Resource Bank</a>
+							</li>
+							<li>
+								<i class="fa fa-comments fa-2x"></i>
+								<a href="">Adullam Chat</a>
+							</li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</div>
 
-				<!-- Search and Notifications -->
-				<div class="">
-					<div class="row">
-						<div class="col-md">
-							<div class="header put-space-between">
-								<!-- Home Search Button -->
-								<div class="col-md-4 v-center">
-									<form action="">
-										<div class="input-group no-search-wrap">
-											<input class="form-control home-search" type="text">
-											<div class="input-group-append">
-												<button class="btn home-search-button" type="button">
+		<!-- Right pane - Content Pane -->
+		<div class="col-md-9 right-pane">
+
+			<!-- Search and Notifications -->
+			<div class="col-md search-notif">
+				<div class="header put-space-between">
+					<!-- Home Search Button -->
+					<div class="col-md-4 v-center">
+						<form action="">
+							<div class="input-group no-search-wrap">
+								<input class="form-control home-search" type="text">
+								<div class="input-group-append">
+									<button class="btn home-search-button" type="button">
 													<i class="fa fa-search"></i>
 												</button>
-											</div>
-										</div>
-									</form>
 								</div>
+							</div>
+						</form>
+					</div>
 
-								<!-- Notifications, Settings, Name and Icon -->
-								<div class="col-md-8">
-									<div class="move-to-right v-center notifs">
-										<a href="chat.php">
+					<!-- Notifications, Settings, Name and Icon -->
+					<div class="col-md-8">
+						<div class="move-to-right v-center notifs">
+							<a href="chat.php">
 											<i class="fa fa-comments fa-2x" style="padding-right: 10px;"></i>
 										</a>
-										<a href="#">
-											<div class="dropdown">
-  												<button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenu2" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+							<a href="#">
+								<div class="dropdown">
+									<button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenu2" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
   													<i class="fa fa-bell fa-2x" style="padding-right: 10px;"></i>
   												</button>
-  												<div class="dropdown-menu" id="notif-dropdown" aria-labelledby="dropdownMenu2">
-													<button class="dropdown-item" type="button">Prayer and fasting</button>
-													<button class="dropdown-item" type="button">Wisdom challenge</button>
-													<button class="dropdown-item" type="button">Wealth Creation Challenge</button>
-													<button class="dropdown-item" type="button">Purpose Challenge</button>
-												</div>
-											</div>
-										</a>
-										<a href="profile.php">
+									<div class="dropdown-menu" id="notif-dropdown" aria-labelledby="dropdownMenu2">
+										<button class="dropdown-item" type="button">Prayer and fasting</button>
+										<button class="dropdown-item" type="button">Wisdom challenge</button>
+										<button class="dropdown-item" type="button">Wealth Creation Challenge</button>
+										<button class="dropdown-item" type="button">Purpose Challenge</button>
+									</div>
+								</div>
+							</a>
+							<a href="profile.php">
 											<i class="fa fa-gear fa-2x " style="padding-right: 10px;"></i>
 										</a>
-										<a href="">
-											<div class="dropdown">
-  												<button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenu2" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+							<a href="">
+								<div class="dropdown">
+									<button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenu2" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
   													<p class="member-name">
   														Jehoshaphat I. abu
   													</p>
   												</button>
-  												<div class="dropdown-menu" id="notif-dropdown" aria-labelledby="dropdownMenu2">
-													<button class="dropdown-item" type="button">Activity Logs</button>
-													<button class="dropdown-item" type="button">Log Out</button>
-												</div>
-											</div>
-										</a>
-										<a href="#">
-											<img style="border-radius: 15px;" src="http://via.placeholder.com/30" alt="">
-										</a>
+									<div class="dropdown-menu" id="notif-dropdown" aria-labelledby="dropdownMenu2">
+										<button class="dropdown-item" type="button">Activity Logs</button>
+										<button class="dropdown-item" type="button">Log Out</button>
 									</div>
 								</div>
-							</div>
+							</a>
+							<a href="#">
+											<img style="border-radius: 15px;" src="http://via.placeholder.com/30" alt="">
+										</a>
 						</div>
 					</div>
 				</div>
-
-				
+			</div>
+		</div>
+	</div>
