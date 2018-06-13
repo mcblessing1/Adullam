dbb952b0f65fc9429d37878c96b8520b8158c8c8
IbukunOluwa Shotubo <mcblessing1@gmail.com>
2018-06-13 17:06:09 +0100

    Done with opportunities, moving to Gallery

---
+++ opportunities.php
@@ -0,0 +1,125 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="description" content="">
+	<meta name="author" content="">
+	<title>Cave Adullam | Opportunities</title>
+
+	<!-- BootStrap 4.1.1 -->
+	<link rel="stylesheet" type="text/css" href="assets/bootstrap/css/bootstrap.min.css">
+
+	<!-- Bootstrap 4.1.1 CSS from CDN -->
+	<!--	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">-->
+
+	<!-- Font Awesome -->
+	<link rel="stylesheet" type="text/css" href="assets/font-awesome/css/font-awesome.min.css">
+
+	<!-- Google Font -->
+	<link href='http://fonts.googleapis.com/css?family=Roboto:400,100,100italic,300,300italic,400italic,500,500italic,700,700italic,900italic,900' rel='stylesheet' type='text/css'>
+
+	<!-- My Stylesheet -->
+	<link rel="stylesheet" href="css/home-style.css">
+	<link rel="stylesheet" href="css/opportunities-style.css">
+
+</head>
+
+
+<body>
+	<!-- Main Content -->
+
+	<!-- php include command to get header and menu from file -->
+	<?php 
+		include 'header+nav.php'; 
+	?>
+	<!-- php include command to get header and menu from file -->
+
+	<div class="container">
+
+		<br>
+
+		<!-- Toggles for display type -->
+		<div class="row">
+			<div class="col">
+				<i class="fa fa-th fa-2x" style="margin-right: 10px; color: #bfbdbd;"></i>
+				<i class="fa fa-bars fa-2x" style="margin-bottom: 20px; color: #bfbdbd;"></i>
+			</div>
+		</div>
+
+		<div class="row">
+			<div class="col-md-8" style="justify-content: flex-start;">
+				<div class="">
+					<img class="text-center img-fluid" src="http://via.placeholder.com/650x500" alt="">
+				</div>
+
+				<p style="background-color: #1f2227; color: white; font-size: 20px; padding: 20px;">Maecenas cursus est sed euismod mattis. Ut interdum convallis lectus, at elementum tellus sagittis ac. Orci varius natoque penatibus et magnis
+				</p>
+
+			</div>
+			<div class="col-md-4">
+				<div class="row">
+					<div class="col-md" style="justify-content: flex-start;">
+						<div class="">
+							<img class="text-center img-fluid" src="http://via.placeholder.com/650x500" alt="">
+						</div>
+
+						<p style="background-color: #1f2227; color: white; font-size: 10px; padding: 20px;">Maecenas cursus est sed euismod mattis.
+						</p>
+
+					</div>
+				</div>
+
+				<div class="row">
+					<div class="col-md" style="justify-content: flex-start;">
+						<div class="">
+							<img class="text-center img-fluid" src="http://via.placeholder.com/650x500" alt="">
+						</div>
+
+						<p style="background-color: #1f2227; color: white; font-size: 10px; padding: 20px;">Maecenas cursus est sed euismod mattis.
+						</p>
+
+					</div>
+				</div>
+				
+				<div class="row">
+					<div class="col-md" style="justify-content: flex-start;">
+						<div class="">
+							<img class="text-center img-fluid" src="http://via.placeholder.com/650x500" alt="">
+						</div>
+
+						<p style="background-color: #1f2227; color: white; font-size: 10px; padding: 20px;">Maecenas cursus est sed euismod mattis.
+						</p>
+
+					</div>
+				</div>
+
+			</div>
+		</div>
+
+
+		<br>
+
+
+
+	</div>
+	</div>
+
+
+	</div>
+
+</body>
+
+
+<!-- BootStrap JQuery, Popper and JS -->
+<!--<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>-->
+<!--<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>-->
+<!--<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T" crossorigin="anonymous"></script>-->
+
+<script src="/assets/jquery/jquery.min.js"></script>
+<script src="/assets/popper/popper.min.js"></script>
+<script src="/assets/bootstrap/js/bootstrap.min.js"></script>
+<script src="/script/home-script.js"></script>
+
+</html>
